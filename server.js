d59ddb1707aab556b4bc780f5c345882c1ddcc6d
Gagan Chauhan <gagan2020chauhan@gmail.com>
2024-08-20 14:55:00 +0530

    created note controller and routes

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 dotenv.config();
 
+import authRouter from "./routes/user.route.js";
+import noteRouter from "./routes/note.route.js";
+
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => {
@@ -22,6 +25,20 @@ app.use(cookieParser());
 app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 app.use(morgan("dev"));
 
+app.use("/api/auth", authRouter);
+app.use("/api/note", noteRouter);
+
 app.listen(process.env.PORT || 8000, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Serer Error";
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
 });
